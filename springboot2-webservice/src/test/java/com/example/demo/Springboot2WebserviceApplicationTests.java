d91db2445a0229e52db17fb59186add44e4abe60
ChoiTaesan <ChoiTaesan@DESKTOP-G9QT27C>
2020-07-12 23:37:04 +0900

    2.3

--- springboot2-webservice/src/test/java/com/example/demo/Springboot2WebserviceApplicationTests.java
+++ springboot2-webservice/src/test/java/com/example/demo/Springboot2WebserviceApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.web.HelloController;
+
+@WebMvcTest(controllers = HelloController.class)
 class Springboot2WebserviceApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+	
 	@Test
-	void contextLoads() {
+	public void hello가_리턴된다() throws Exception {
+		String hello = "hello";
+		
+		mockMvc.perform(get("/hello"))
+	    .andExpect(status().isOk())
+		.andExpect(content().string(hello));
+
+
+		
 	}
 
 }
